16a7679be9b34d587e369003e10643d217710d8b
Thibaud Ruelle <thibaud.ruelle@gmail.com>
2020-10-29 10:48:23 +0100

    Add correction parameters and emptying buffer before getting spectra

--- qcodes_contrib_drivers/drivers/OceanOptics/QEPro.py
+++ qcodes_contrib_drivers/drivers/OceanOptics/QEPro.py
@@ -1,12 +1,16 @@
 try:
+    import seabreeze
+    #seabreeze.use('pyseabreeze')
     import seabreeze.spectrometers as sb
     from seabreeze.spectrometers import Spectrometer
 except ImportError:
     raise ImportError('Could not find seabreeze module.')
 
 from qcodes.instrument.base import Instrument
-from qcodes import ArrayParameter, MultiParameter, ManualParameter
+from qcodes import ArrayParameter, MultiParameter, ManualParameter, ParameterWithSetpoints
 from qcodes.utils import validators as vals
+
+import numpy as np
 
 class QEPro(Instrument):
     """
@@ -28,14 +32,43 @@ class QEPro(Instrument):
                            set_cmd=lambda x: self._spec.integration_time_micros(x)
                            )
 
+        self.add_parameter(name='correct_dark_counts',
+                           label='correct dark counts boolean',
+                           vals=vals.Bool(),
+                           initial_value=False,
+                           parameter_class=ManualParameter
+                           )
+
+        self.add_parameter(name='correct_nonlinearity',
+                           label='correct nonlinearity boolean',
+                           vals=vals.Bool(),
+                           initial_value=False,
+                           parameter_class=ManualParameter
+                           )
+
         self.add_parameter(name='wavelengths',
                            label='wavelength',
                            unit='nm',
-                           get_cmd=lambda: self._spec.wavelengths()
+                           get_cmd=self._spec.wavelengths,
+                           vals=vals.Arrays(shape=(len(self._wl_nm_list),))
                            )
 
         self.add_parameter(name='intensities',
-                           label='intensity',
+                           label='Intensity',
                            unit='a.u.',
-                           get_cmd=lambda: self._spec.intensities()
+                           get_cmd=self._get_fresh_intensities,
+                           vals=vals.Arrays(shape=(len(self._wl_nm_list),))
                            )
+
+        self.add_parameter(name='spectrum',
+                           label='Spectrum',
+                           unit='a.u.',
+                           parameter_class=ParameterWithSetpoints,
+                           setpoints=(self.wavelengths,),
+                           get_cmd=self._get_fresh_intensities,
+                           vals=vals.Arrays(shape=(len(self._wl_nm_list),)) 
+                           )
+
+    def _get_fresh_intensities(self):
+        self._spec.f.data_buffer.clear()
+        return self._spec.intensities(correct_dark_counts=self.correct_dark_counts(), correct_nonlinearity=self.correct_nonlinearity())
